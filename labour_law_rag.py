7ea990cc93d0b63e17c476a9c6ea753d259f5a3e
AbaHarrison <abaharrison18@gmail.com>
2025-10-19 13:55:54 +0000

    update requirement with text splitter in labour law file

--- labour_law_rag.py
+++ labour_law_rag.py
@@ -3,7 +3,7 @@ import logging
 from typing import List, Dict, Optional
 from pathlib import Path
 import pickle
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
